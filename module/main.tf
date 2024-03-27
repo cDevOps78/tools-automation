c03c7e24b93b8ce0bb96b2f27e49ff79292972a9
chaitu1 <chaitu143@gmail.com>
2024-03-27 22:22:59 +0530

    spot-request setup for prometheus

--- module/main.tf
+++ module/main.tf
@@ -2,16 +2,20 @@ resource "aws_instance" "instance" {
   ami                      = var.ami_m
   instance_type            = var.instance_type_m
   vpc_security_group_ids   = var.vpc_security_group_ids_m
-  spot_options             = {
-    instance_interruption_behavior = "stop"
-    spot_instance_type             = "persistent"
+  tags                     = var.tags_m
+
+  instance_market_options  = {
+    spot_options = {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
   }
-  tags          = var.tags_m
+
 }
 resource "aws_route53_record" "record" {
-  name    = "${var.tags_m.Name}.chaithanya.online"
-  type    = "A"
-  zone_id = "Z03998933DKS43BUYGV0L"
-  ttl     = 30
-  records = [aws_instance.instance.public_ip]
+  name              = "${var.tags_m.Name}.chaithanya.online"
+  type              = "A"
+  zone_id           = "Z03998933DKS43BUYGV0L"
+  ttl               = 30
+  records           = [aws_instance.instance.public_ip]
 }
