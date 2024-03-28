b00cd60d0b09cb318d49b144eb7246a68f9bd1ff
chaitu1 <chaitu143@gmail.com>
2024-03-28 08:25:29 +0530

    prometheus ansible setup is done

--- ansibleTools.sh
+++ ansibleTools.sh
@@ -1,4 +1,3 @@
 toolName=$1
-roleName=$2
 
-ansible-playbook -i ${toolName}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${roleName} toolsRoleCall.yaml
+ansible-playbook -i ${toolName}.chaithanya.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${toolName} toolsRoleCall.yamlba
